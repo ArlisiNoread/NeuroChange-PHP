8b5415fd8e1f9b9cfe9d2c79d1214db2c1f0cddf
ArlisiNoread <41275777+ArlisiNoread@users.noreply.github.com>
2018-08-16 23:38:05 -0500

    Landing Page

--- aviso-privacidad.php
+++ aviso-privacidad.php
@@ -1,147 +1,132 @@
-<?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
+<!DOCTYPE html>
+<html lang = "en">
 
-if (!isset($_SESSION["formFilled"])) {
-    include './index.php';
-} else if (!$_SESSION["formFilled"]) {
-    include './index.php';
-} else {
-    ?>   
-
-    <!DOCTYPE html>
-    <html lang = "en">
-
-        <head>
-            <meta charset = "utf-8">
-            <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
-            <title>Neurochange</title>
-            <link rel="icon" href="./assets/img/logo/favicon.ico" type="image/x-icon" />
-            <link href = "https://necolas.github.io/normalize.css/8.0.0/normalize.css" rel = "stylesheet">
-            <link rel = "icon" href = "./assets/img/logo/favicon.ico" type = "image/x-icon" />
-            <link rel = "stylesheet" href = "assets/bootstrap/css/bootstrap.css">
-            <link rel = "stylesheet" href = "assets/fonts/font-awesome.min.css">
-            <link rel = "stylesheet" href = "assets/fonts/ionicons.min.css">
-            <link rel = "stylesheet" href = "https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity = "sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin = "anonymous">
-            <link href = "https://fonts.googleapis.com/css?family=Cabin:400,700" rel = "stylesheet">
-            <link href = "https://fonts.googleapis.com/css?family=Oswald:400,700" rel = "stylesheet">
-            <link href = "https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel = "stylesheet">
-            <link rel = "stylesheet" href = "./assets/css/aviso-privacidad.css">
-            <link rel = "stylesheet" href = "./assets/css/responsividad-aviso-privacidad.css">
-            <link href = "https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel = "stylesheet">
+    <head>
+        <meta charset = "utf-8">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+        <title>Neurochange</title>
+        <link rel="icon" href="./assets/img/logo/favicon.ico" type="image/x-icon" />
+        <link href = "https://necolas.github.io/normalize.css/8.0.0/normalize.css" rel = "stylesheet">
+        <link rel = "icon" href = "./assets/img/logo/favicon.ico" type = "image/x-icon" />
+        <link rel = "stylesheet" href = "assets/bootstrap/css/bootstrap.css">
+        <link rel = "stylesheet" href = "assets/fonts/font-awesome.min.css">
+        <link rel = "stylesheet" href = "assets/fonts/ionicons.min.css">
+        <link rel = "stylesheet" href = "https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity = "sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin = "anonymous">
+        <link href = "https://fonts.googleapis.com/css?family=Cabin:400,700" rel = "stylesheet">
+        <link href = "https://fonts.googleapis.com/css?family=Oswald:400,700" rel = "stylesheet">
+        <link href = "https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel = "stylesheet">
+        <link rel = "stylesheet" href = "./assets/css/aviso-privacidad.css">
+        <link rel = "stylesheet" href = "./assets/css/responsividad-aviso-privacidad.css">
+        <link href = "https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel = "stylesheet">
 
 
 
-        </head>
+    </head>
 
-        <body>
-
-
-            <div class = "envolvente">
-
-                <nav id = "barraNavegacion" class = "navbar navbar-light navbar-expand-md">
-                    <div class = "container-fluid">
-
-                        <a class = "navbar-brand" href = "./index.php"><img class = "logo" src = "assets/img/logo/Logo.png" /></a>
-
-                    </div>
-                </nav>
+    <body>
 
 
-                <div class = "container-fluid borde-aviso-privacidad">
-                    <div class = "logo-img">
-                        <img class = "logo-gris" src = "assets/img/logo/LogoCortado.png" />
-                    </div>
+        <div class = "envolvente">
 
-                    <div id="config-texto" class = "pegado-derecha margin-configuracion">
+            <nav id = "barraNavegacion" class = "navbar navbar-light navbar-expand-md">
+                <div class = "container-fluid">
 
-                        <h5><b>¿QUIÉNES SOMOS?</b></h5>
-                        <p class = "">Somos una empresa dedicada a la capacitación y al desarrollo de ejecutivos y directivos, enfocados a potencializar sus capacidades. Nuestra empresa se denomina <b>NEUROCHANGE, S. DE R.L. DE C.V.</b> (en lo sucesivo <b>NEUROCHANGE CONSULTING</b>) con domicilio en Heriberto Frías número 1439 interior 402 Colonia: Del Valle, Delegación: Benito Juárez, Código Postal 03100 en la Ciudad de México, antes Distrito Federal. Somos responsables de recabar, tratar y proteger sus datos personales con las finalidades establecidas en el presente documento, así como por lo dispuesto en la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, su reglamento y demás disposiciones legales aplicables.</p>
+                    <a class = "navbar-brand" href = "./index.php"><img class = "logo" src = "assets/img/logo/Logo.png" /></a>
 
-                        <h5><b>¿QUE DATOS PERSONALES RECABAMOS?</b></h5>
-
-                        <p>En <b>NEUROCHANGE CONSULTING</b>, recabamos sus datos personales de forma personal, directa o indirecta, a través de distintos medios, tales como medios electrónicos, sonoros, visuales, digitales u otros medios permitidos por la ley, que permitan el tratamiento de sus datos personales.</p>
-                        <ul>
-                            <li>1. Nombre.</li>
-                            <li>2. Teléfono fijo y/o móvil.</li>
-                            <li>3. Domicilio.</li>
-                            <li>4. Correo electrónico.</li>
-                            <li>5. Fecha de Nacimiento.</li>
-                            <li>6. Nombre de la fuente de trabajo en donde presta sus servicios.</li>
-                            <li>7. Puesto que desempeña en la fuente de trabajo.</li>
-                            <li>8. Grado Académico.</li>
-                            <li>9. Sexo.</li>
-                            <li>10. Experiencia profesional.</li>
-                        </ul>
-                        <h5><b>B) Si usted es proveedor. Los datos personales que someteremos a tratamiento son los siguientes:</b></h5>
-                        <ul>
-                            <li>1. Nombre.</li>
-                            <li>2. Teléfono.</li>
-                            <li>3. Domicilio.</li>
-                            <li>4. Correo electrónico.</li>
-                            <li>5. R.F.C.</li>
-                            <li>6. Datos bancarios.</li>
-                        </ul>
-
-                        <h5><b>¿CON QUÉ FINALIDADES SOMETEREMOS A TRATAMIENTO SUS DATOS PERSONALES?</b></h5>
-                        <P>Las finalidades bajo las cuales someteremos a tratamiento los datos personales serán para:</P>
-                        <h5><b>A) Si usted es cliente o usuario de algunos de nuestros productos y/o servicios</b></h5>
-                        <ul>
-                            <li>1.- Ofrecer y promocionar nuestros servicios profesionales en materia de capacitación y desarrollo profesional.</li>
-                            <li>2.- Prestar los servicios profesionales en materia de capacitación y desarrollo profesional.</li>
-                            <li>3.- Enviar cotizaciones de honorarios de nuestros servicios profesionales.</li>
-                            <li>4.- Emitir facturas.</li>
-                            <li>5.- Mantener contacto con Usted.</li>
-                            <li>6.- Prestar servicios de atención al cliente.</li>
-                        </ul>
-                        <h5><b>B) Si usted es proveedor.</b></h5>
-                        <ul>
-                            <li>1.- Verificar sus datos personales.</li>
-                            <li>2.- Efectuar pagos por los productos y/o servicios contratados con Usted.</li>
-                            <li>3.- Efectuar pagos por los productos y/o servicios contratados con Usted.</li>
-                            <li>4.- Mantener contacto con Usted..</li>
-                        </ul>
-                        <h5><b>¿CÓMO PODEMOS LIMITAR EL USO O DIVULGACIÓN DE SUS DATOS PERSONALES?</b></h5>
-                        <p>Si usted desea limitar el uso o la divulgación de sus datos personales, así como dejar de recibir información referente acerca del ofrecimiento y promoción de nuestros servicios profesionales, por favor contáctese con nosotros enviando un escrito libre con su solicitud a la dirección de correo electrónico <a href = "mailto:avisodeprivacidad@neurochangeconsulting.com.mx"><b>avisodeprivacidad@neurochangeconsulting.com</b></a></p>
-
-                        <h5><b>¿CÓMO PROTEGEMOS SUS DATOS PERSONALES?</b></h5>
-
-                        <p>La confidencialidad de sus datos personales se encuentra garantizada y protegida por las medidas de seguridad administrativas, técnicas y físicas, con las que contamos en <b>NEUROCHANGE CONSULTING</b>, para evitar cualquier daño, pérdida, alteración, divulgación, uso, acceso o tratamiento no autorizado de los mismos. Únicamente el personal autorizado de <b>NEUROCHANGE CONSULTING</b> tendrá acceso a sus datos personales.</p>
-
-                        <h5><b>TRASNFERIMOS SUS DATOS PERSONALES A TERCEROS?</b></h5>
-                        <p>Le informamos que sus datos personales no serán transmitidos a ningún tercero ajeno a <b>NEUROCHANGE CONSULTING</b> salvo que usted nos otorgue su consentimiento verbal y/o por escrito.</p>
-
-                        <h5><b>¿CÓMO PUEDE EJERCER SUS DERECHOS DE ACCESO, RECTIFICACIÓN, CANCELACIÓN Y OPOSICIÓN (ARCO) AL TRATAMIENTO DE LOS DATOS PERSONALES?</b></h5>
-
-                        <p>Si usted desea ejercer los derechos ARCO antes mencionados es necesario presentar un escrito, a la dirección de correo electrónico <a href = "mailto:avisodeprivacidad@neurochangeconsulting.com.mx"><b>avisodeprivacidad@neurochangeconsulting.com.mx</b></a> o al domicilio ubicado en Heriberto Frías número 1439 interior 402 Colonia: Del Valle, Delegación: Benito Juárez, Código Postal 03100 en la México, Distrito Federal que contenga la siguiente información:</p>
-                        <ul>
-                            <li>1. El derecho que desea ejercer.</li>
-                            <li>2. Su nombre completo.</li>
-                            <li>3. El (los) documento (s) que acredite (n) su identidad o la de su representante legal.</li>
-                            <li>4. Una descripción clara y precisa de sus datos personales y los elementos o documentos que faciliten su localización.</li>
-                        </ul>
-
-                        <h5><b>¿DÓNDE PUBLICAMOS LAS MODIFICACIONES AL AVISO DE PRIVACIDAD?</b></h5>
-                        <p>Asimismo, le informamos que el presente Aviso de Privacidad puede ser modificado en cualquier momento por <b>NEUROCHANGE CONSULTING</b> dichas modificaciones se harán de su conocimiento a través del correo electrónico, teléfono, celular, o en el sitio de internet <a href = "http://www.neurochangeconsulting.com/avisodeprivacidad.php"><b>http://www.neurochangeconsulting.com/avisodeprivacidad.php</b></a> así como en cualquier otro medio de comunicación que en <b>NEUROCHANGE CONSULTING</b> determinemos para tal efecto.</p>
-                        <br>
+                </div>
+            </nav>
 
 
-                    </div>
+            <div class = "container-fluid borde-aviso-privacidad">
+                <div class = "logo-img">
+                    <img class = "logo-gris" src = "assets/img/logo/LogoCortado.png" />
+                </div>
+
+                <div id="config-texto" class = "pegado-derecha margin-configuracion">
+
+                    <h5><b>¿QUIÉNES SOMOS?</b></h5>
+                    <p class = "">Somos una empresa dedicada a la capacitación y al desarrollo de ejecutivos y directivos, enfocados a potencializar sus capacidades. Nuestra empresa se denomina <b>NEUROCHANGE, S. DE R.L. DE C.V.</b> (en lo sucesivo <b>NEUROCHANGE CONSULTING</b>) con domicilio en Heriberto Frías número 1439 interior 402 Colonia: Del Valle, Delegación: Benito Juárez, Código Postal 03100 en la Ciudad de México, antes Distrito Federal. Somos responsables de recabar, tratar y proteger sus datos personales con las finalidades establecidas en el presente documento, así como por lo dispuesto en la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, su reglamento y demás disposiciones legales aplicables.</p>
+
+                    <h5><b>¿QUE DATOS PERSONALES RECABAMOS?</b></h5>
+
+                    <p>En <b>NEUROCHANGE CONSULTING</b>, recabamos sus datos personales de forma personal, directa o indirecta, a través de distintos medios, tales como medios electrónicos, sonoros, visuales, digitales u otros medios permitidos por la ley, que permitan el tratamiento de sus datos personales.</p>
+                    <ul>
+                        <li>1. Nombre.</li>
+                        <li>2. Teléfono fijo y/o móvil.</li>
+                        <li>3. Domicilio.</li>
+                        <li>4. Correo electrónico.</li>
+                        <li>5. Fecha de Nacimiento.</li>
+                        <li>6. Nombre de la fuente de trabajo en donde presta sus servicios.</li>
+                        <li>7. Puesto que desempeña en la fuente de trabajo.</li>
+                        <li>8. Grado Académico.</li>
+                        <li>9. Sexo.</li>
+                        <li>10. Experiencia profesional.</li>
+                    </ul>
+                    <h5><b>B) Si usted es proveedor. Los datos personales que someteremos a tratamiento son los siguientes:</b></h5>
+                    <ul>
+                        <li>1. Nombre.</li>
+                        <li>2. Teléfono.</li>
+                        <li>3. Domicilio.</li>
+                        <li>4. Correo electrónico.</li>
+                        <li>5. R.F.C.</li>
+                        <li>6. Datos bancarios.</li>
+                    </ul>
+
+                    <h5><b>¿CON QUÉ FINALIDADES SOMETEREMOS A TRATAMIENTO SUS DATOS PERSONALES?</b></h5>
+                    <P>Las finalidades bajo las cuales someteremos a tratamiento los datos personales serán para:</P>
+                    <h5><b>A) Si usted es cliente o usuario de algunos de nuestros productos y/o servicios</b></h5>
+                    <ul>
+                        <li>1.- Ofrecer y promocionar nuestros servicios profesionales en materia de capacitación y desarrollo profesional.</li>
+                        <li>2.- Prestar los servicios profesionales en materia de capacitación y desarrollo profesional.</li>
+                        <li>3.- Enviar cotizaciones de honorarios de nuestros servicios profesionales.</li>
+                        <li>4.- Emitir facturas.</li>
+                        <li>5.- Mantener contacto con Usted.</li>
+                        <li>6.- Prestar servicios de atención al cliente.</li>
+                    </ul>
+                    <h5><b>B) Si usted es proveedor.</b></h5>
+                    <ul>
+                        <li>1.- Verificar sus datos personales.</li>
+                        <li>2.- Efectuar pagos por los productos y/o servicios contratados con Usted.</li>
+                        <li>3.- Efectuar pagos por los productos y/o servicios contratados con Usted.</li>
+                        <li>4.- Mantener contacto con Usted..</li>
+                    </ul>
+                    <h5><b>¿CÓMO PODEMOS LIMITAR EL USO O DIVULGACIÓN DE SUS DATOS PERSONALES?</b></h5>
+                    <p>Si usted desea limitar el uso o la divulgación de sus datos personales, así como dejar de recibir información referente acerca del ofrecimiento y promoción de nuestros servicios profesionales, por favor contáctese con nosotros enviando un escrito libre con su solicitud a la dirección de correo electrónico <a href = "mailto:avisodeprivacidad@neurochangeconsulting.com.mx"><b>avisodeprivacidad@neurochangeconsulting.com</b></a></p>
+
+                    <h5><b>¿CÓMO PROTEGEMOS SUS DATOS PERSONALES?</b></h5>
+
+                    <p>La confidencialidad de sus datos personales se encuentra garantizada y protegida por las medidas de seguridad administrativas, técnicas y físicas, con las que contamos en <b>NEUROCHANGE CONSULTING</b>, para evitar cualquier daño, pérdida, alteración, divulgación, uso, acceso o tratamiento no autorizado de los mismos. Únicamente el personal autorizado de <b>NEUROCHANGE CONSULTING</b> tendrá acceso a sus datos personales.</p>
+
+                    <h5><b>TRASNFERIMOS SUS DATOS PERSONALES A TERCEROS?</b></h5>
+                    <p>Le informamos que sus datos personales no serán transmitidos a ningún tercero ajeno a <b>NEUROCHANGE CONSULTING</b> salvo que usted nos otorgue su consentimiento verbal y/o por escrito.</p>
+
+                    <h5><b>¿CÓMO PUEDE EJERCER SUS DERECHOS DE ACCESO, RECTIFICACIÓN, CANCELACIÓN Y OPOSICIÓN (ARCO) AL TRATAMIENTO DE LOS DATOS PERSONALES?</b></h5>
+
+                    <p>Si usted desea ejercer los derechos ARCO antes mencionados es necesario presentar un escrito, a la dirección de correo electrónico <a href = "mailto:avisodeprivacidad@neurochangeconsulting.com.mx"><b>avisodeprivacidad@neurochangeconsulting.com.mx</b></a> o al domicilio ubicado en Heriberto Frías número 1439 interior 402 Colonia: Del Valle, Delegación: Benito Juárez, Código Postal 03100 en la México, Distrito Federal que contenga la siguiente información:</p>
+                    <ul>
+                        <li>1. El derecho que desea ejercer.</li>
+                        <li>2. Su nombre completo.</li>
+                        <li>3. El (los) documento (s) que acredite (n) su identidad o la de su representante legal.</li>
+                        <li>4. Una descripción clara y precisa de sus datos personales y los elementos o documentos que faciliten su localización.</li>
+                    </ul>
+
+                    <h5><b>¿DÓNDE PUBLICAMOS LAS MODIFICACIONES AL AVISO DE PRIVACIDAD?</b></h5>
+                    <p>Asimismo, le informamos que el presente Aviso de Privacidad puede ser modificado en cualquier momento por <b>NEUROCHANGE CONSULTING</b> dichas modificaciones se harán de su conocimiento a través del correo electrónico, teléfono, celular, o en el sitio de internet <a href = "http://www.neurochangeconsulting.com/avisodeprivacidad.php"><b>http://www.neurochangeconsulting.com/avisodeprivacidad.php</b></a> así como en cualquier otro medio de comunicación que en <b>NEUROCHANGE CONSULTING</b> determinemos para tal efecto.</p>
+                    <br>
 
 
                 </div>
 
+
             </div>
 
-            <script src = "assets/js/jquery.min.js"></script>
-            <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        </div>
+
+        <script src = "assets/js/jquery.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 
 
-        </body>
+    </body>
 
-    </html>
+</html>
 
-    <?php
-}
-?>
